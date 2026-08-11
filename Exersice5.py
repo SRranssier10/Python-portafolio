bcf7d8400db176b08d64b380c7d512a84d29e6df
SRranssier10 <eliamdelbois@gmail.com>
2026-08-11 15:40:55 -0400

    add exercise 5

---
+++ Exersice5.py
@@ -0,0 +1,15 @@
+#1. Control variables
+correct_password = "eliam123"
+remaining_attempts = 3
+#2. Validation loop
+while remaining_attempts > 0:
+    entered_password = input("Enter the password: ")
+    if entered_password == correct_password:
+        print("Access granted. Welcome to the system!")
+        break # Exit the loop immediately
+    else:
+        remainin_attempts -= 1
+        if remaining_attempts > 0:
+            print(f"Incorrect password. You have {remaining_attempts} attempt(s) left.\n")
+        else:
+            print("account locked due to too many failed attempts.")
